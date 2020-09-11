c4d7016c6cbe26962b17d8cf46dce079157717ae
tttxdxd <2602656110@qq.com>
2020-09-11 14:42:16 +0800

    Add files via upload

---
+++ js/redirect.7863e6e0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["redirect"],{ef3c:function(e,r,t){"use strict";t.r(r);t("ac1f"),t("5319");var n,u,a={created:function(){var e=this.$route,r=e.params,t=e.query,n=r.path;console.log({path:"/"+n,query:t}),this.$router.replace({path:"/"+n,query:t})},render:function(e){return e()}},c=a,o=t("2877"),p=Object(o["a"])(c,n,u,!1,null,null,null);r["default"]=p.exports}}]);
+//# sourceMappingURL=redirect.7863e6e0.js.map
